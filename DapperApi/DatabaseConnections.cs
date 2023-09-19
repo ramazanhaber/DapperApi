dab312083d59079d9ebf62060ce74b432214879b
Ramazan HABER <ramazanhaberr@gmail.com>
2023-09-20 02:13:08 +0300

    aa

--- DapperApi/DatabaseConnections.cs
+++ DapperApi/DatabaseConnections.cs
@@ -1,17 +1,14 @@
 ﻿using System.Data;
-
 namespace DapperApi
 {
     public class DatabaseConnections
     {
         public IDbConnection DefaultConnection { get; }
         public IDbConnection SecondConnection { get; }
-
         public DatabaseConnections(IDbConnection defaultConnection, IDbConnection secondConnection)
         {
             DefaultConnection = defaultConnection;
             SecondConnection = secondConnection;
         }
     }
-
 }
